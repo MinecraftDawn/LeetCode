cbaeeb8971d1fed93ba7d034a0c3d6a481239f85
MinecraftDawn <eric4902077@gmail.com>
2019-06-14 21:45:37 +0800

    第四十二題(優化)

--- Hard/42. Trapping Rain Water.py
+++ Hard/42. Trapping Rain Water.py
@@ -1,24 +1,20 @@
 class Solution:
     def trap(self, height: list) -> int:
         if not height: return 0
-        leftList = []
         rightList = []
         leftH = height[0]
         rightH = height[len(height)-1]
         sumWater = 0
         
-        for c,h in enumerate(height):
-            if h > leftH:
-                leftH = h
-            leftList.append(leftH)
-            
         for c in range(len(height)-1,-1,-1):
             h = height[c]
             if h > rightH:
                 rightH = h
             rightList.insert(0,rightH)
         
-        for c,val in enumerate(height):
-            sumWater += min(leftList[c],rightList[c]) - val
+        for c,h in enumerate(height):
+            if h > leftH:
+                leftH = h
+            sumWater += min(leftH,rightList[c]) - h
         
         return sumWater
