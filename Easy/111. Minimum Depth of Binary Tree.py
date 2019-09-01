971a4125eabfb60da36a5cc9362ed26b9ab9151b
MinecraftDawn <eric4902077@gmail.com>
2019-09-01 11:27:41 +0800

    第一百一十一題

---
+++ Easy/111. Minimum Depth of Binary Tree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if not root: return 0
+        
+        if not root.left and not root.right:
+            return 1
+        
+        elif not root.left:
+            return self.minDepth(root.right) + 1
+        
+        elif not root.right:
+            return self.minDepth(root.left) + 1
+        
+        else:
+            return min(self.minDepth(root.left)+1,
+                       self.minDepth(root.right)+1)
