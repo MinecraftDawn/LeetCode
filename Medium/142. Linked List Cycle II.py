facdb9eeae95708cacf8cf0450c849be35c63bcf
MinecraftDawn <eric4902077@gmail.com>
2019-10-05 11:23:23 +0800

    第一百四十二題

---
+++ Medium/142. Linked List Cycle II.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        visted = {}
+        run = head
+        
+        while run:
+            if run not in visted:
+                visted[run] = True
+            else:
+                return run
+            run = run.next
+            
+        return None
