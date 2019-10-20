2f3ed2bf7af04a0cb0223f2f112efe0f299d1fb6
MinecraftDawn <eric4902077@gmail.com>
2019-10-20 16:35:49 +0800

    第一百六十題(改用set代替dict)

---
+++ Easy/160. Intersection of Two Linked Lists(set).py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        visted = set()
+        run = headA
+        
+        while run:
+            visted.add(run)
+            run = run.next
+            
+        run = headB
+        while run:
+            if run in visted:
+                return run
+            run = run.next
+        return None
