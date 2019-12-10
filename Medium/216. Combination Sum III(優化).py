a6cb0a2066ef243770187efd593d1c6f66b23935
MinecraftDawn <eric4902077@gmail.com>
2019-12-10 19:40:17 +0800

    第兩百一十六題(優化)

--- Medium/216. Combination Sum III(優化).py
+++ Medium/216. Combination Sum III(優化).py
@@ -7,13 +7,11 @@ class Solution:
     def dfs(self, k, curNum, usedNum:list):
         if k == 1:
             if curNum not in usedNum and 0 < curNum < 10:
-                usedNum.append(curNum)
-                usedNum.sort()
-                self.ans.add(tuple(usedNum))        
+                self.ans.add(tuple(sorted(usedNum+[curNum])))
         else:
             start = max(usedNum)+1 if usedNum else 1
             for i in range(start, 10):
                 if curNum - i > 0:
-                    passNums = usedNum.copy()
-                    passNums.append(i)
-                    self.dfs(k-1, curNum-i, passNums)
+                    usedNum.append(i)
+                    self.dfs(k-1, curNum-i, usedNum)
+                    usedNum.pop()
