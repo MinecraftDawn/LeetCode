bf61d4544960b317313af40359224080843fbb63
MinecraftDawn <eric4902077@gmail.com>
2019-12-27 11:11:18 +0800

    第兩百三十二題

---
+++ Easy/232. Implement Queue using Stacks.py
@@ -0,0 +1,35 @@
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.queue = []
+        
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.queue.append(x)
+        
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        return self.queue.pop(0)
+        
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        return self.queue[0]
+        
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return not self.queue
