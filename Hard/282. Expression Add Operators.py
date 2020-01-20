983290057aed3d67a8c3fd9d94ebb02bf648216c
MinecraftDawn <eric4902077@gmail.com>
2020-01-21 00:45:42 +0800

    第兩百八十二題(錯誤)

--- Hard/282. Expression Add Operators.py
+++ Hard/282. Expression Add Operators.py
@@ -7,9 +7,14 @@ class Solution:
         
     def calculate(self, num: str, pos:int):
         if pos >= len(num):
-            if eval(num) == self.target:
-                self.ans.append(num)
+            try:
+                if eval(num) == self.target:
+                    self.ans.append(num)
+            except:
+                pass
         else:
             self.calculate(num[:pos] + "+" + num[pos:], pos+2)
             self.calculate(num[:pos] + "-" + num[pos:], pos+2)
             self.calculate(num[:pos] + "*" + num[pos:], pos+2)
+            if pos+1 < len(num) and num[pos+1] != '0':
+                self.calculate(num[:pos] + num[pos:], pos+1)
