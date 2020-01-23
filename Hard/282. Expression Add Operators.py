eaedf47a06bedb5ccfc1735e56e42ea573c83e88
MinecraftDawn <eric4902077@gmail.com>
2020-01-23 21:43:04 +0800

    第兩百八十二題

--- Hard/282. Expression Add Operators.py
+++ Hard/282. Expression Add Operators.py
@@ -1,20 +1,23 @@
+# Reference: https://leetcode.com/problems/expression-add-operators/discuss/71900/18-line-clean-Python-solution-DFS-with-comment
 class Solution:
     def addOperators(self, num: str, target: int) -> list:
         self.ans = []
         self.target = target
-        self.calculate(num, 1)
+        self.calculate(num, "", 0, 0)
         return self.ans
         
-    def calculate(self, num: str, pos:int):
-        if pos >= len(num):
-            try:
-                if eval(num) == self.target:
-                    self.ans.append(num)
-            except:
-                pass
+    def calculate(self, num: str, curStr: str, curVal: int, pre:int):
+        if not num:
+            if curVal == self.target:
+                self.ans.append(curStr)
         else:
-            self.calculate(num[:pos] + "+" + num[pos:], pos+2)
-            self.calculate(num[:pos] + "-" + num[pos:], pos+2)
-            self.calculate(num[:pos] + "*" + num[pos:], pos+2)
-            if pos+1 < len(num) and num[pos+1] != '0':
-                self.calculate(num[:pos] + num[pos:], pos+1)
+            for i in range(1, len(num)+1):
+                if i > 1 and num[0] == '0': continue
+            
+                if not curStr:
+                    self.calculate(num[i:], num[:i], int(num[:i]), int(num[:i]))
+                
+                else:
+                    self.calculate(num[i:], curStr + "+" + num[:i], curVal + int(num[:i]), int(num[:i]))
+                    self.calculate(num[i:], curStr + "-" + num[:i], curVal - int(num[:i]), -int(num[:i]))
+                    self.calculate(num[i:], curStr + "*" + num[:i], curVal - pre + pre * int(num[:i]), pre * int(num[:i]))
