5e71816d3e9dcae41fec3f980126db7c2fb3f506
MinecraftDawn <eric4902077@gmail.com>
2020-02-25 17:33:15 +0800

    第三百一十九題

--- Medium/319. Bulb Switcher.py
+++ Medium/319. Bulb Switcher.py
@@ -1,10 +1,3 @@
 class Solution:
     def bulbSwitch(self, n: int) -> int:
-        if n == 0: return 0
-        
-        ans = 1
-        for i in range(2, n):
-            if i*i <= n:
-                ans += 1
-                
-        return ans
+        return int(n**0.5)
