ac5c49301fd44f1d980b5e15060f0f50556a7ca9
MinecraftDawn <eric4902077@gmail.com>
2020-02-29 20:32:00 +0800

    第三百二十七題(超時)

--- Hard/327. Count of Range Sum.py
+++ Hard/327. Count of Range Sum.py
@@ -1,9 +1,15 @@
 class Solution:
     def countRangeSum(self, nums: list, lower: int, upper: int) -> int:
-        ans = []
+        dp = [0]
+        total = 0
+        for num in nums:
+            total += num
+            dp.append(total)
+        
+        ans = 0
         for i in range(len(nums)):
             for j in range(i+1, len(nums)+1):
-                if lower <= sum(nums[i:j]) <= upper:
-                    ans.append([i,j])
-                    
-        return len(ans)
+                if lower <= dp[j]-dp[i] <= upper:
+                    ans += 1          
+
+        return ans
