3b7970e78d1f180179b00528469bde5e38faeb73
MinecraftDawn <eric4902077@gmail.com>
2020-03-22 19:21:43 +0800

    ²ĤT¦ʤT¤Q¤»ÃD

--- Hard/336. Palindrome Pairs.py
+++ Hard/336. Palindrome Pairs.py
@@ -1,16 +1,26 @@
+# Reference: https://leetcode.com/problems/palindrome-pairs/discuss/535904/Python-3-Clean-Solutions
 class Solution:
     def palindromePairs(self, words: list) -> list:
         def palindrome(word:str) -> bool:
-            if word == word[::-1]:
-                return True
-            else:
-                return False
+            return word == word[::-1]
         
         ans = []
         
-        for i in range(len(words)):
-            for j in range(len(words)):
-                if i != j and palindrome(words[i]+words[j]):
-                    ans.append([i,j])
-                    
+        table = {}
+        for i, word in enumerate(words):
+            table[word] = i
+        
+        for index, word in enumerate(words):
+            for j in range(len(word)+1):
+                w1, w2 = word[:j], word[j:]
+                if palindrome(w1):
+                    rev = w2[::-1]
+                    if rev in table and table[rev] != index:
+                        ans.append([table[rev], index])
+                
+                if w2 and palindrome(w2):
+                    rev = w1[::-1]
+                    if rev in table and table[rev] != index:
+                        ans.append([index, table[rev]])
+        
         return ans
