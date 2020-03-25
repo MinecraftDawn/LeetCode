e450af0e2c71c0f42cd7601e424a649b6e6b394a
MinecraftDawn <eric4902077@gmail.com>
2020-03-25 10:49:40 +0800

    第三百三十八題(使用bit運算)

--- Medium/338. Counting Bits.py
+++ Medium/338. Counting Bits.py
@@ -1,7 +1,7 @@
 class Solution:
     def countBits(self, num: int) -> list:
-        ans = []
-        for i in range(num+1):
-            ans.append(bin(i).count("1"))
+        ans = [0]
+        for i in range(1, num+1):
+            ans.append(ans[i>>1] + (i&1))
             
         return ans
