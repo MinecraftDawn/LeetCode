cf5545a0380aad7423e10fd24635c52a73fca17c
MinecraftDawn <eric4902077@gmail.com>
2020-04-06 11:33:41 +0800

    ²ĤT¦ʤ­¤QÃD Àu¤Æ

--- Easy/350. Intersection of Two Arrays II.py
+++ Easy/350. Intersection of Two Arrays II.py
@@ -2,11 +2,6 @@ from collections import Counter
 class Solution:
     def intersect(self, nums1: list, nums2: list) -> list:
         count1 = Counter(nums1)
-        ans = []
+        count2 = Counter(nums2)
         
-        for num in nums2:
-            if count1.get(num):
-                count1[num] -= 1
-                ans.append(num)
-                
-        return ans
+        return list((count1&count2).elements())
