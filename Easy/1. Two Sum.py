56c69f42e2b0084495381ea7972fbda830af3771
MinecraftDawn <eric4902077@gmail.com>
2020-04-08 22:57:53 +0800

    第一題

--- Easy/1. Two Sum.py
+++ Easy/1. Two Sum.py
@@ -1,6 +1,7 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
+    def twoSum(self, nums: list, target: int) -> list:
+        numsMap = {num:index for index,num in enumerate(nums)}
+        
+        for index,num in enumerate(nums):
+            if target-num in numsMap and numsMap[target-num] != index:
+                return [index, numsMap[target-num]]
