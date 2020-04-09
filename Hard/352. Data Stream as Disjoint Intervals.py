41321a9c3dd25009d4b0773b45c8e815a4b89551
MinecraftDawn <eric4902077@gmail.com>
2020-04-09 22:15:11 +0800

    第三百五十二題(還是錯誤)

--- Hard/352. Data Stream as Disjoint Intervals.py
+++ Hard/352. Data Stream as Disjoint Intervals.py
@@ -4,32 +4,30 @@ class SummaryRanges:
         """
         Initialize your data structure here.
         """
-        self.stream = []
+        self.stream = [[float('-inf'),float('-inf')],
+                       [float('inf'),float('inf')]]
         
 
     def addNum(self, val: int) -> None:
         left = bisect.bisect_left(self.stream, [val])
         right = bisect.bisect_right(self.stream, [val])
-        if left == 0:
-            self.stream.insert(0, [val,val])
-        elif right == len(self.stream):
-            self.stream.append([val,val])
-        else:
-            flag = 0
-            if self.stream[left-1][1] == val-1:
-                self.stream[left-1][1] = val
-                flag += 1
-            
-            if self.stream[right][0] == val+1:
-                self.stream[right][0] = val
-                flag += 1
+        flag = 0
+        if self.stream[left-1][1] == val-1:
+            self.stream[left-1][1] = val
+            flag += 1
+        
+        if self.stream[right][0] == val+1:
+            self.stream[right][0] = val
+            flag += 1
 
-            if flag == 2:
-                self.stream[left-1][1] = self.stream[right][1]
-                self.stream.pop(right)
+        if flag == 2:
+            self.stream[left-1][1] = self.stream[right][1]
+            self.stream.pop(right)
+        elif flag == 0:
+            bisect.insort_left(self.stream, [val,val])
 
     def getIntervals(self) -> list:
-        return self.stream
+        return self.stream[1:-1]
         
 
 
