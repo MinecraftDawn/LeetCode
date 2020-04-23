f25df554d06036d71a9eb92fa74dd7d40ec563b9
MinecraftDawn <eric4902077@gmail.com>
2020-04-23 14:36:56 +0800

    第三百六十三題(DP超時)

--- Hard/363. Max Sum of Rectangle No Larger Than K.py
+++ Hard/363. Max Sum of Rectangle No Larger Than K.py
@@ -3,6 +3,12 @@ class Solution:
         self.matrix = matrix
         row = len(matrix)
         col = len(matrix[0])
+        self.dp = [[0] * (col+1) for _ in range(row)]
+        
+        for i in range(row):
+            for j in range(col):
+                self.dp[i][j] = self.dp[i][j-1] + matrix[i][j]
+        
         ans = float("-inf")
         
         for r in range(row):
@@ -18,7 +24,6 @@ class Solution:
     def getSum(self, x1, y1, x2, y2) -> int:
         ans = 0
         for i in range(x1, x2+1):
-            for j in range(y1, y2+1):
-                ans += self.matrix[i][j]
+            ans += self.dp[i][y2] - self.dp[i][y1-1]
         
         return ans
