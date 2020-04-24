6ccf0032922ec6425b472d2f234cbc11c1597de1
MinecraftDawn <eric4902077@gmail.com>
2020-04-24 20:05:00 +0800

    第三百六十三題(參考)

--- Hard/363. Max Sum of Rectangle No Larger Than K.py
+++ Hard/363. Max Sum of Rectangle No Larger Than K.py
@@ -1,3 +1,5 @@
+# Reference: https://leetcode.com/problems/max-sum-of-rectangle-no-larger-than-k/discuss/233938/Python-code-with-comments-explaining-the-algorithm
+import bisect
 class Solution:
     def maxSumSubmatrix(self, matrix: list, k: int) -> int:
         if not matrix or not matrix[0]: return 0
@@ -8,29 +10,22 @@ class Solution:
         if row > col:
             matrix = list(zip(*matrix))
             row,col = col,row
-            
-        self.matrix = matrix
-        self.dp = [[0] * (col+1) for _ in range(row+1)]
-        
-        for i in range(row):
-            for j in range(col):
-                self.dp[i+1][j+1] = self.dp[i][j+1] + self.dp[i+1][j] + matrix[i][j] - self.dp[i][j]
-        
-        for i in self.dp:
-            print(i)
         
         ans = float("-inf")
         
-        for r in range(row):
-            for c in range(col):
-                for i in range(row-r):
-                    for j in range(col-c):
-                        curSum = self.getSum(i,j,i+r,j+c)
-                        if curSum <= k:
-                            ans = max(ans, curSum)
-                            
+        for start in range(row):
+            sums = [0] * col
+            for end in range(start, row):
+                slist = []
+                num = 0
+                for y in range(col):
+                    sums[y] += matrix[end][y]
+                    num += sums[y]
+                    if num <= k:
+                        ans = max(ans, num)
+                    index = bisect.bisect_left(slist, num-k)
+                    if index != len(slist):
+                        ans = max(ans, num-slist[index])
+                    bisect.insort(slist, num)
+
         return ans
-                    
-    def getSum(self, x1, y1, x2, y2) -> int:
-        return self.dp[x2+1][y2+1] - self.dp[x1][y2+1] - self.dp[x2+1][y1] + self.dp[x1][y1]
-                   
