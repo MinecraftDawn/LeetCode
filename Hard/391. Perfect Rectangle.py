0a81154b856d15de611664dda92e225a22bfe6c0
MinecraftDawn <eric4902077@gmail.com>
2020-05-27 19:56:41 +0800

    第三百九十一題

---
+++ Hard/391. Perfect Rectangle.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isRectangleCover(self, rectangles: list) -> bool:
+        rectRange = [[float("inf"), float("inf")], [float("-inf"), float("-inf")]]
+        sumArea = 0
+        for rect in rectangles:
+            rectRange[0] = min(rectRange[0], rect[:2])
+            rectRange[1] = max(rectRange[1], rect[2:])
+
+            sumArea += (rect[2] - rect[0]) * (rect[3] - rect[0])
+
+        return (rectRange[1][0] - rectRange[0][0]) * (rectRange[1][1] - rectRange[0][1]) == sumArea
