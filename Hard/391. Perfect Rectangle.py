852bc31dc607e750969f3667e0cda86da47c85ab
MinecraftDawn <eric4902077@gmail.com>
2020-05-29 15:13:33 +0800

    第三百九十一題(wrong answer)

--- Hard/391. Perfect Rectangle.py
+++ Hard/391. Perfect Rectangle.py
@@ -5,6 +5,9 @@ class Solution:
                      [float("-inf"), float("inf")],
                      [float("-inf"), float("-inf")]]
         sumArea = 0
+
+        angleSet = set()
+
         for rect in rectangles:
             rectRange[0] = min(rectRange[0], rect[:2])
             if rect[0] <= rectRange[1][0] and rect[3] >= rectRange[1][1]:
@@ -13,6 +16,19 @@ class Solution:
                 rectRange[2] = [rect[2], rect[1]]
             rectRange[3] = max(rectRange[3], rect[2:])
 
+            angles = [(rect[0], rect[1]),
+                     (rect[0], rect[3]),
+                     (rect[2], rect[1]),
+                     (rect[2], rect[3])]
+
+            for angle in angles:
+                if angle in angleSet:
+                    angleSet.remove(angle)
+                else:
+                    angleSet.add(angle)
+
             sumArea += (rect[2] - rect[0]) * (rect[3] - rect[1])
+
+        if len(angleSet) != 4: return False
 
         return (rectRange[3][0] - rectRange[0][0]) * (rectRange[3][1] - rectRange[0][1]) == (rectRange[2][0] - rectRange[1][0]) * (rectRange[1][1] - rectRange[2][1]) == sumArea
