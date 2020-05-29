66955b995bee27f117e68b6c0cc5f8abfa71e148
MinecraftDawn <eric4902077@gmail.com>
2020-05-29 14:40:04 +0800

    第三百九十一題(wrong answer)

--- Hard/391. Perfect Rectangle.py
+++ Hard/391. Perfect Rectangle.py
@@ -6,6 +6,6 @@ class Solution:
             rectRange[0] = min(rectRange[0], rect[:2])
             rectRange[1] = max(rectRange[1], rect[2:])
 
-            sumArea += (rect[2] - rect[0]) * (rect[3] - rect[0])
+            sumArea += (rect[2] - rect[0]) * (rect[3] - rect[1])
 
         return (rectRange[1][0] - rectRange[0][0]) * (rectRange[1][1] - rectRange[0][1]) == sumArea
