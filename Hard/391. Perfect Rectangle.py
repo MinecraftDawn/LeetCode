d9f40f7e2d63173b6cd889b6c84b8e6f33d0c6e8
MinecraftDawn <eric4902077@gmail.com>
2020-05-29 14:59:22 +0800

    第三百九十一題(wrong answer)

--- Hard/391. Perfect Rectangle.py
+++ Hard/391. Perfect Rectangle.py
@@ -1,11 +1,18 @@
 class Solution:
     def isRectangleCover(self, rectangles: list) -> bool:
-        rectRange = [[float("inf"), float("inf")], [float("-inf"), float("-inf")]]
+        rectRange = [[float("inf"), float("inf")],
+                     [float("inf"),float("-inf")],
+                     [float("-inf"), float("inf")],
+                     [float("-inf"), float("-inf")]]
         sumArea = 0
         for rect in rectangles:
             rectRange[0] = min(rectRange[0], rect[:2])
-            rectRange[1] = max(rectRange[1], rect[2:])
+            if rect[0] <= rectRange[1][0] and rect[3] >= rectRange[1][1]:
+                rectRange[1] = [rect[0], rect[3]]
+            if rect[2] >= rectRange[2][0] and rect[1] <= rectRange[2][1]:
+                rectRange[2] = [rect[2], rect[1]]
+            rectRange[3] = max(rectRange[3], rect[2:])
 
             sumArea += (rect[2] - rect[0]) * (rect[3] - rect[1])
 
-        return (rectRange[1][0] - rectRange[0][0]) * (rectRange[1][1] - rectRange[0][1]) == sumArea
+        return (rectRange[3][0] - rectRange[0][0]) * (rectRange[3][1] - rectRange[0][1]) == (rectRange[2][0] - rectRange[1][0]) * (rectRange[1][1] - rectRange[2][1]) == sumArea
