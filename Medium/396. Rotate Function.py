682c495c5bb393610b1cb7781610dab7682569a7
MinecraftDawn <eric4902077@gmail.com>
2020-06-15 15:39:15 +0800

    第三百九十六題

--- Medium/396. Rotate Function.py
+++ Medium/396. Rotate Function.py
@@ -1,12 +1,17 @@
 class Solution:
     def maxRotateFunction(self, A: list) -> int:
         ans = float("-inf")
+
+        ASum = sum(A)
+        dp = [self.F(A)]
         for i in range(len(A)):
-            ans = max(ans, self.F(A[i:] + A[:i]))
+            curF = dp[-1] + ASum - len(A) * A[-i - 1]
+            dp.append(curF)
+            ans = max(ans, curF)
 
         return ans if ans != float("-inf") else 0
 
-    def F(self, A:list):
+    def F(self, A: list):
         value = 0
         for i in range(len(A)):
             value += i * A[i]
