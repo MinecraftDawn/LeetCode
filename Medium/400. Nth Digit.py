b8e50053ff5d3ca33cb8315211e8ace32eec46ed
MinecraftDawn <eric4902077@gmail.com>
2020-06-24 14:08:47 +0800

    第四百題

--- Medium/400. Nth Digit.py
+++ Medium/400. Nth Digit.py
@@ -1,7 +1,17 @@
+from math import ceil
 class Solution:
     def findNthDigit(self, n: int) -> int:
-        nums = []
-        for i in range(1, n+1):
-            nums += list(str(i))
-            
-        return nums[n-1]
+        nums = 0
+        i = 1
+        while nums < n:
+            nums += i * 9 * 10 **(i-1)
+            i += 1
+        i -= 1
+        nums -= i * 9 * 10 **(i-1)
+        
+        n -= nums
+        
+        t1 = 10**(i-1) + ceil(n / i) - 1
+        t2 = (n-1) % i
+        
+        return int(str(t1)[t2])
