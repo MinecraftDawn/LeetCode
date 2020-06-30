b81380624186f99ddbe3dcb8e6a6bfcb970fe660
MinecraftDawn <eric4902077@gmail.com>
2020-07-01 00:03:01 +0800

    第四百零三題(自訂快取)

--- Hard/403. Frog Jump(LRU_Cache).py
+++ Hard/403. Frog Jump(LRU_Cache).py
@@ -1,24 +1,21 @@
+from functools import lru_cache
+
+
 class Solution:
     def canCross(self, stones: list) -> bool:
         if stones[0] != 0 or stones[1] != 1: return False
 
         self.end = stones[-1]
         self.stones = set(stones)
-        self.visted = {}
 
         return self.jump(0, 1)
 
+    @lru_cache(None)
     def jump(self, cur: int, step: int):
         if cur == self.end: return True
 
-        if (cur, step) in self.visted:
-            return self.visted[(cur, step)]
-
         for i in range(max(step - 1, 1), step + 2):
-            if cur + i in self.stones:
-                arrival = self.jump(cur + i, i)
-                self.visted[(cur + i, i)] = arrival
-                if arrival:
-                    return True
+            if cur + i in self.stones and self.jump(cur + i, i):
+                return True
 
         return False
