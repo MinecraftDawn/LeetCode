b5efdbef7b273ef47418cd03cf813d072b6a967d
MinecraftDawn <eric4902077@gmail.com>
2020-07-02 19:41:59 +0800

    第四百零五題

--- Easy/405. Convert a Number to Hexadecimal.py
+++ Easy/405. Convert a Number to Hexadecimal.py
@@ -1,6 +1,11 @@
 class Solution:
     def toHex(self, num: int) -> str:
         if num < 0:
-            return hex(2**32+num)[2:]
-        else:
-            return hex(num)[2:]
+            return self.toHex(2 ** 32 + num)
+
+        numMap = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
+        ans = ""
+        while num > 0 or not ans:
+            ans = numMap[num % 16] + ans
+            num //= 16
+        return ans
