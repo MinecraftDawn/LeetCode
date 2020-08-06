15cc7f8fc59da0f84ceb049c198f392d400ea8e5
MinecraftDawn <eric4902077@gmail.com>
2020-08-06 16:46:21 +0800

    第四百一十三題

--- Medium/413. Arithmetic Slices.py
+++ Medium/413. Arithmetic Slices.py
@@ -1,14 +1,23 @@
 class Solution:
     def numberOfArithmeticSlices(self, A: list) -> int:
         ans = 0
-        for i in range(2, len(A)):
+        dp = []
+        for i in range(len(A) - 1):
+            dp.append(A[i + 1] - A[i])
 
-            for j in range(1, len(A) - i+1):
-                diff = A[j] - A[j - 1]
-                for k in range(j+1, j + i):
-                    if A[k] - A[k-1] != diff:
-                        break
-                else:
-                    ans += 1
+        dp.append(float("-inf"))
+        pre = float("inf")
+        count = 0
+
+        for i in range(len(dp)):
+            if pre == dp[i]:
+                count += 1
+            else:
+                if count > 1:
+                    count -= 1
+                    ans += count * (count + 1) // 2
+
+                pre = dp[i]
+                count = 1
 
         return ans
