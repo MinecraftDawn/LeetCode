1e9a5c4919e09ea1b8a7f4ca01867afc579db02e
MinecraftDawn <eric4902077@gmail.com>
2020-08-23 16:23:29 +0800

    第四百一十七題

--- Medium/417. Pacific Atlantic Water Flow.py
+++ Medium/417. Pacific Atlantic Water Flow.py
@@ -4,36 +4,31 @@ class Solution:
         self.matrix = matrix
         self.rows = len(self.matrix)
         self.cols = len(self.matrix[0])
-        ans = []
-        self.hasArr = [[[False, False] for _ in range(self.cols)] for _ in range(self.rows)]
+        pacific = set()
+        atlantic = set()
 
-        for row in range(self.rows):
-            for col in range(self.cols):
-                visited = [[False for _ in range(self.cols)] for _ in range(self.rows)]
-                visited[row][col] = True
-                if self.dfs(row, col, visited) == [True, True]:
-                    ans.append((row, col))
+        for i in range(self.cols):
+            pacific.add((0, i))
+            self.dfs(0, i, pacific)
+            atlantic.add((self.rows-1, i))
+            self.dfs(self.rows-1, i, atlantic)
 
-        return ans
+        for i in range(self.rows):
+            pacific.add((i, 0))
+            self.dfs(i, 0, pacific)
+            atlantic.add((i, self.cols-1))
+            self.dfs(i, self.cols-1, atlantic)
+        
+        return pacific & atlantic
 
-    def dfs(self, row: int, col: int, visited: list):
-        curVal = self.matrix[row][col]
-        for i, j in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+    def dfs(self, row:int, col:int, ocean:set):
+        value = self.matrix[row][col]
+
+        for i,j in [(1,0), (-1,0), (0,1), (0,-1)]:
             nxtRow = row + i
             nxtCol = col + j
-            if nxtRow < 0 or nxtCol < 0:
-                self.hasArr[row][col][0] = True
-            elif nxtRow >= self.rows or nxtCol >= self.cols:
-                self.hasArr[row][col][1] = True
-            else:
-                nxtVal = self.matrix[nxtRow][nxtCol]
-                if nxtVal <= curVal:
-                    if visited[nxtRow][nxtCol]:
-                        t1, t2 = self.hasArr[nxtRow][nxtCol]
-                        self.hasArr[row][col] = [self.hasArr[row][col][0] or t1, self.hasArr[row][col][1] or t2]
-                    else:
-                        visited[nxtRow][nxtCol] = True
-                        t1, t2 = self.dfs(nxtRow, nxtCol, visited)
-                        self.hasArr[row][col] = [self.hasArr[row][col][0] or t1, self.hasArr[row][col][1] or t2]
-
-        return self.hasArr[row][col]
+            if 0 <= nxtRow < self.rows and 0 <= nxtCol < self.cols:
+                nxtValue = self.matrix[nxtRow][nxtCol]
+                if value <= nxtValue and (nxtRow, nxtCol) not in ocean:
+                    ocean.add((nxtRow, nxtCol))
+                    self.dfs(nxtRow, nxtCol, ocean)
