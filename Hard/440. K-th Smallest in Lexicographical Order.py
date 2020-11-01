876fac6258fd6d9596f5ddff45bfeecc0b7677e0
MinecraftDawn <eric4902077@gmail.com>
2020-11-01 20:59:15 +0800

    第四百四十題(超時)

--- Hard/440. K-th Smallest in Lexicographical Order.py
+++ Hard/440. K-th Smallest in Lexicographical Order.py
@@ -1,3 +1,18 @@
+from collections import defaultdict
+
+
 class Solution:
     def findKthNumber(self, n: int, k: int) -> int:
-        return sorted(list(map(str, range(1, n + 1))))[k - 1]
+        hashMap = defaultdict(list)
+
+        for i in range(1, n + 1):
+            i = str(i)
+            hashMap[i[0]].append(i)
+
+        i = 1
+        while k > 0:
+            if len(hashMap[str(i)]) >= k:
+                return sorted(hashMap[str(i)])[k - 1]
+            else:
+                k -= len(hashMap[str(i)])
+                i += 1
