0404bcce27ff363430e6ab71dbc27a69055fd261
MinecraftDawn <eric4902077@gmail.com>
2021-06-14 13:57:09 +0800

    第三百零四題，使用積分圖

--- Medium/304. Range Sum Query 2D - Immutable.py
+++ Medium/304. Range Sum Query 2D - Immutable.py
@@ -1,26 +1,12 @@
 class NumMatrix:
 
     def __init__(self, matrix: list):
-        self.dp = []
-        for _ in range(len(matrix)): self.dp.append([0])
-        
-        for i in range(len(matrix)):
-            tmp = 0
-            for num in matrix[i]:
-                tmp += num
-                self.dp[i].append(tmp)
-        for i in self.dp:
-            print(i)
-        
+        self.dp = [[0] * (len(matrix[0]) + 1) for _ in range(len(matrix) + 1)]
+
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                self.dp[row + 1][col + 1] = matrix[row][col] + self.dp[row + 1][col] + self.dp[row][col + 1] - \
+                                            self.dp[row][col]
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        ans = 0
-        for i in range(row1, row2+1):
-            ans += self.dp[i][col2+1] - self.dp[i][col1]
-        return ans
-        
-
-
-# Your NumMatrix object will be instantiated and called as such:
-# obj = NumMatrix(matrix)
-# param_1 = obj.sumRegion(row1,col1,row2,col2)
+        return self.dp[row2 + 1][col2 + 1] - self.dp[row2 + 1][col1] - self.dp[row1][col2 + 1] + self.dp[row1][col1]
